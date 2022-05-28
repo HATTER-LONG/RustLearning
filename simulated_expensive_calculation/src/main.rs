6ded127c88a0ba5cea3ea1b0144c1d87b3f59774
Layton <caolei6767@gmail.com>
2022-05-28 15:42:02 +0800

    finish 11

--- simulated_expensive_calculation/src/main.rs
+++ simulated_expensive_calculation/src/main.rs
@@ -64,4 +64,133 @@ mod tests {
         let v2 = c.value(2);
         assert_eq!(v2, 2);
     }
+
+    #[test]
+    fn call_itertor() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+
+        for val in v1_iter {
+            println!("Got : {}", val);
+        }
+    }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        // 调用 next 方法时，相当于改变迭代器中的记录 pos 的数值了
+        // 因此 迭代器 为 mut
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_map() {
+        let v1 = vec![1, 2, 3];
+        // 迭代器是惰性的，如果不调用消耗型适配器方法那么 map 捕获做任何事情。
+        // v1.iter().map(|x| x + 1);
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|x| x.size == shoe_size).collect()
+}
+
+#[test]
+fn filter_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ]
+    )
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
+
+#[test]
+fn using_other_iterator_trait_method() {
+    // zip 拉链，可以将两个迭代器合并到一起，结果是一个元组，其中到元素就是原先到两个迭代器
+    // map 的参数是 zip 产生的元组，其分别对应的就是两个迭代器的值
+    // filter 参数过滤掉其中不能被三整除的值
+    // 最红 sum 进行遍历求和
+    let sum: u32 = Counter::new()
+        .zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b)
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
 }
