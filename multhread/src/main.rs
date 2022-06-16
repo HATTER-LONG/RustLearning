60ad6f4909a53fcc76cc7d3ae440e79b7d295d54
Layton <caolei6767@gmail.com>
2022-06-16 22:01:10 +0800

    finish 14

---
+++ multhread/src/main.rs
@@ -0,0 +1,13 @@
+use std::thread;
+
+fn main() {
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    // drop(v);
+    //      ^ value used here after move
+
+    handle.join().unwrap();
+}
